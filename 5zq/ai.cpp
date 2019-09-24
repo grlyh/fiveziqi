c3b504dff6ba400b9ea66a32f838ddc9299a8adb
ikka <49584845+kasumi-ikka@users.noreply.github.com>
2019-09-24 10:00:27 +0000

    Update ai.cpp

--- 5zq/ai.cpp
+++ 5zq/ai.cpp
@@ -69,29 +69,54 @@ class AI_1 : public AI {
         }
       }
     }
-    // player huo 3
-    int h3x, h3y, max_h3_num = 0;
+    // huo 3 & tiao 3
     for (int i = 0; i < board_size; ++i) {
       for (int j = 0; j < board_size; ++j) if (chess_board[i][j] == -1) {
-        int h3_num = 0;
         for (int k = 0; k < 8; k += 2) {
-          int res = 0;
-          for (int l = 1; l < 5; ++l) {
-            int x = i + l * dx[k], y = j + l * dy[k];
-            if (!inboard(x, y) || chess_board[x][y] != (color ^ 1)) break;
-            ++res;
+          for (int g = 0; g < 2; ++g) {
+            int res = 0, cnum = 0, f = 0;
+            for (int l = 1; l < 5; ++l) {
+              int x = i + l * dx[k], y = j + l * dy[k];
+              if (!inboard(x, y) || chess_board[x][y] != (color ^ g)) {
+                if (res && inboard(x, y) &&
+                    chess_board[x][y] == (color ^ g ^ 1) &&
+                    chess_board[x - dx[k]][y - dy[k]] == (color ^ g)) f = 1;
+                break;
+              }
+              res++, cnum++;
+            }
+            res = 0;
+            for (int l = 1; l < 5; ++l) {
+              int x = i + l * dx[k + 1], y = j + l * dy[k + 1];
+              if (!inboard(x, y) || chess_board[x][y] != (color ^ g)) {
+                if (res && inboard(x, y) &&
+                    chess_board[x][y] == (color ^ g ^ 1) &&
+                    chess_board[x - dx[k]][y - dy[k]] == (color ^ g)) f = 1;
+                break;
+              }
+              res++, cnum++;
+            }
+            if (cnum < 3) f = 1;
+            res = 1;
+            for (int l = 1; l < 5; ++l) {
+              int x = i + l * dx[k], y = j + l * dy[k];
+              if (!inboard(x, y) || chess_board[x][y] == (color ^ g ^ 1)) break;
+              res++;
+            }
+            for (int l = 1; l < 5; ++l) {
+              int x = i + l * dx[k], y = j + l * dy[k];
+              if (!inboard(x, y) || chess_board[x][y] == (color ^ g ^ 1)) break;
+              res++;
+            }
+            if (res < 5) f = 1;
+            if (!f) return (void) (chess_board[i][j] = color);
           }
-          for (int l = 1; l < 5; ++l) {
-            int x = i + l * dx[k + 1], y = j + l * dy[k + 1];
-            if (!inboard(x, y) || chess_board[x][y] != (color ^ 1)) break;
-            ++res;
-          }
-          if (res >= 3) ++h3_num;
         }
-        if (max_h3_num < h3_num) max_h3_num = h3_num, h3x = i, h3y = j;
       }
     }
-    if (max_h3_num) return (void) (chess_board[h3x][h3y] = color);
+    // ai
+
+    //
     int movex, movey, max_score = -1;
     for (int i = 0; i < board_size; ++i) {
       for (int j = 0; j < board_size; ++j) if (chess_board[i][j] == -1) {
@@ -99,7 +124,7 @@ class AI_1 : public AI {
         for (int k = 0; k < 8; ++k) {
           for (int l = 1; l < board_size; ++l) {
             int x = i + l * dx[k], y = j + l * dy[k];
-            if (!inboard(x, y) || (chess_board[x][y] == (color ^ 1)) * ((unsigned)rand() % 100 < 80)) break;
+            if (!inboard(x, y) || (chess_board[x][y] != color) * ((unsigned)rand() % 100 < 00)) break;
             score += 10 * (chess_board[x][y] == color);
           }
         }
@@ -121,9 +146,11 @@ class AI_SKH : public AI {
   }
   void move() {
     int movex, movey, max_score = -1;
-    for (int i = 0; i < board_size; ++i) {
-      for (int j = 0; j < board_size; ++j) if (chess_board[i][j] == -1) {
-        int score = s_score[i][j];
+    int sti=rand()&1;
+    for (int XX = 0; XX < board_size; ++XX) {
+    int i=sti?XX:board_size-XX+1,stj=rand()*rand()&1;
+      for(int YY = 0,j=stj?YY:board_size-YY+1; YY < board_size; ++YY,j=stj?YY:board_size-YY+1) if (chess_board[i][j] == -1) {
+    int score = s_score[i][j],f2=0,f3=0;
         for(int k=0;k<8;k+=2){
           int MX=i,MY=j,TOT=0;
           for(int l=1;l<=4;++l){
@@ -139,8 +166,11 @@ class AI_SKH : public AI {
             if(chess_board[MX][MY]==color)TOT++;
             else if(l>1 && chess_board[MX+dx[k]][MY+dy[k]]==-1)break;
       }
+      if(f2){if(TOT==2)score+=400,f2=0;}
+      else if(TOT==2)f2=1;
+      if(f3){if(TOT==3)score+=400,f3=0;}
+      else if(TOT==3)f3=1;
       score+=mVAL[TOT];
-      
       MX=i,MY=j,TOT=0;
       for(int l=1;l<=4;++l){
             MX+=dx[k],MY+=dy[k];
@@ -222,8 +252,7 @@ const int AI_SKH::s_score[board_size][board_size] = {
   {0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0}, 
   {0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0}, 
   {0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0}, 
-  {0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0}, 
-  {0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0}, 
+  {0, 0, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 0, 0}, 
   {0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0}, 
   {0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0}, 
   {0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0}, 
@@ -232,9 +261,10 @@ const int AI_SKH::s_score[board_size][board_size] = {
   {0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0}, 
   {0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0}, 
   {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, 
+  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, 
 };
 
-const int AI_SKH::mVAL[] = {0, 5, 10, 25, 100, 100, 100, 100, 100};
-const int AI_SKH::aVAL[] = {0, 5, 10, 100, 100, 100, 100, 100, 100};
+const int AI_SKH::mVAL[] = {0, 5, 10, 101, 1001, 1001, 1001, 1001, 1001};
+const int AI_SKH::aVAL[] = {0, 5, 10, 102, 1002, 1002, 1002, 1002, 1002};
 
 #endif
